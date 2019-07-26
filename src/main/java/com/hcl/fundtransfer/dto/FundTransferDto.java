7ac52f495aae0d1bd9f2c8adac3df046a5e83691
amoljadhav531 <amoljadhav531@gmail.com>
2019-07-26 15:22:43 +0530

    fund transfer dto/FundTransferDto.java

--- src/main/java/com/hcl/fundtransfer/dto/FundTransferDto.java
+++ src/main/java/com/hcl/fundtransfer/dto/FundTransferDto.java
@@ -3,49 +3,23 @@ package com.hcl.fundtransfer.dto;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+import lombok.Data;
 
 /**
  * FundTransfer class used as request body model
+ * 
  * @author amol.jadhav
  */
+@Data
 public class FundTransferDto {
 
 	@NotNull
-	@NotBlank
-	private String loginUserName;
-	
+	private long userId;
+
 	@NotNull
-	private PayeeDto payee;
-	
+	private long payeeId;
+
 	@NotNull
 	private double amount;
-	
 
-
-	public String getLoginUserName() {
-		return loginUserName;
-	}
-
-	public void setLoginUserName(String loginUserName) {
-		this.loginUserName = loginUserName;
-	}
-
-	public PayeeDto getPayee() {
-		return payee;
-	}
-
-	public void setPayee(PayeeDto payee) {
-		this.payee = payee;
-	}
-
-	public double getAmount() {
-		return amount;
-	}
-
-	public void setAmount(double amount) {
-		this.amount = amount;
-	}
-	
-	
-	
 }
