80e2e2e948e1efa3b05a55920d80acdaaeb2e638
amoljadhav531 <amoljadhav531@gmail.com>
2019-07-26 14:26:14 +0530

    Added dto/UserDetailsDto.java

---
+++ src/main/java/com/hcl/fundtransfer/dto/UserDetailsDto.java
@@ -0,0 +1,56 @@
+package com.hcl.fundtransfer.dto;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.Data;
+
+/**
+ * User class represent the user table in database
+ * @author amol.jadhav
+ */
+@Data
+public class UserDetailsDto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long userId;
+	
+	@NotNull
+	@NotBlank
+	@Length(min = 5, max = 15)
+	private String username;
+	
+	@NotNull
+	@NotBlank
+	private String password;
+	
+	@NotNull
+	@NotBlank
+	private String fullName;
+	
+	@NotNull
+	private long mobileNumber;
+	
+	@NotNull
+	@NotBlank
+	private String email;
+	
+	@NotNull
+	@NotBlank
+	private String gender;
+	
+	@NotNull
+	@NotBlank
+	private String dob;
+	
+}
